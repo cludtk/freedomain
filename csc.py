ba5b939ba4891d74f3b4adcde45d385823510e8a
cludtk <xclud@outlook.com>
2015-08-02 21:28:31 +0300

    Add csc type

--- csc.py
+++ csc.py
@@ -1,13 +1,51 @@
 __author__ = 'cvl'
 
 
-class CSC(dict):
+def numberic_system(base_system):
+    result = {}
+    for csc_n in base_system:
+        result[csc_n] = base_system.find(csc_n)
+    return result
+
+
+class CSC(object):
+    system = {}
 
     def __init__(self, alphabet):
-        return numbering_system(alphabet)
+        self.system = numberic_system(alphabet)
 
-    def numbering_system(self, base_system):
-        result = {}
-        for csc_n in base_system:
-            result[csc_n] = base_system.find(csc_n)
+    def csc(self, sym):
+        result = ''
+        for s in sym:
+            result += str(self.system[s])
         return result
+
+    def r_csc(self, num):
+        for key in self.system.keys():
+            if self.system[key] == int(num):
+                return key
+        return 'out_of_range'
+
+    def increment(self, csc_number):
+        csc_len = len(csc_number)
+        i = 0
+        while 1:
+            if i > csc_len:
+                csc_number += '0'
+            if i == csc_len:
+                csc_number += '0'
+                break
+
+            num = csc_number[i]
+            if num in self.system.keys():
+                csc_result = self.r_csc(int(self.csc(num)) + 1)
+                if csc_result != 'out_of_range':
+                    csc_number = csc_number[:i] + csc_result + csc_number[i + 1:]
+                    break
+                else:
+                    csc_number = csc_number[:i] + '0' + csc_number[i + 1:]
+                    i += 1
+            else:
+                csc_number = csc_number[:i] + '0' + csc_number[i + 1:]
+                i += 1
+        return csc_number
